0f05233be29bdd4830f822369b2b5ab3a8f9674c
Ahnder <skguss1@gmail.com>
2019-12-30 16:21:52 +0900

    setting 7 actions - Show, Edit, Update, Destroy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 var app = express();
 
@@ -41,6 +42,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 // urlencoded data 를 extended 알고리즘을 사용해서 분석한다
 app.use(bodyParser.urlencoded({ extended: true }));
+// _method 의 query 로 들어오는 값으로 HTTP method를 바꾼다
+// ex) http://example.com/category/id?_method=delete 를 받으면 _method 값인 delete 를 읽어
+//     해당 request 의 HTTP method 를 delete 로 바꾼다
+app.use(methodOverride('_method'));
 
 // mongoose.Schema 함수로 DB에서 사용할 schema를 설정한다.
 // 데이터베이스에 정보를 어떠한 형식으로 저장할지를 지정해주는 부분
@@ -103,6 +108,53 @@ app.get('/contacts/new', (req, res) => {
 // 에러없이 contact data 가 생성되면 /contacts로 redirect 한다
 app.post('/contacts', (req, res) => {
     Contact.create(req.body, (err, contact) => {
+        if (err) return res.json(err);
+        res.redirect('/contacts');
+    });
+});
+// Contacts - Show
+// "contacts/:id" 에 get 요청이 오는 경우
+// :id 처럼 route 에 콜론(:)을 사용하면 해당 위치의 값을 받아 req.params 에 넣게 된다
+// 예를들어 "contacts/asd123" 이 입력되면 "contacts/:id" route 에서 이를 받아 
+// req.params.id 에 "asd123" 을 넣게 된다
+// Model.findOnd 은 DB에서 해당 model 의 document 를 하나 찾는 함수이다
+// 첫 번째 parameter로 찾을 조건을 object로 입력하고 data를 찾은 후 callback함수를 호출한다
+// Model.find는 조건에 맞는 결과를 모두 찾아 array로 전달하는데 비해
+// Model.findOne은 조건에 맞는 결과를 하나 찾아 object로 전달한다(검색결과가 없다면 null 전달)
+// 아래의 코드에서는 {_id:req.params.id}를 조건으로 전달하고 있는데, 
+// 즉 DB의 contacts collection 에서 _id가 req.params.id 와 일치하는 data를 찾는 조건이다
+// 에러가 없다면 검색 결과를 받아 views/contacts/show.ejs를 render 한다 
+app.get('/contacts/:id', (req, res) => {
+    Contact.findOne({_id:req.params.id}, (err, contact) => {
+        if (err) return res.json(err);
+        res.render('contacts/show', {contact:contact});
+    });
+});
+// Contacts - Edit
+app.get('/contacts/:id/edit', (req, res) => {
+    Contact.findOne({_id:req.params.id}, (err, contact) => {
+        if (err) return res.json(err);
+        res.render('contacts/edit', {contact:contact});
+    });
+});
+// Contacts - Update
+// Model.findOneAndUpdate 는 DB에서 해당 model 의 document 를 하나 찾아 그 data 를 수정하는 함수다
+// 첫 번째 parameter 로 찾을 조건을 object 로 입력하고
+// 두 번째 parameter 로 update 할 정보를 object 로 입력, data 를 찾은 후 
+// callback함수를 호출한다
+// 이때 callback함수로 넘겨지는 값은 수정되기 전의 값이다
+// 만약 업데이트된 후의 값을 보고 싶다면 callback함수 전에 parameter로 {new:true}를 넣어주면 된다
+app.put('/contacts/:id', (req, res) => {
+    Contact.findOneAndUpdate({_id:req.params.id}, req.body, (err, contact) => {
+        if (err) return res.json(err);
+        res.redirect('/contacts/' + req.params.id);
+    });
+});
+// Contacts - destroy
+// Model.deleteOne 은 DB 에서 해당 model의 document 를 하나 찾아 삭제하는 함수다
+// 첫 번쨰 parameter 로 찾을 조건을 입력하고 data 를 찾은 후 callback함수를 호출한다
+app.delete('/contacts/:id', (req, res) => {
+    Contact.deleteOne({_id:req.params.id}, err => {
         if (err) return res.json(err);
         res.redirect('/contacts');
     });
